54e9661db679d498410cdffeffd64398f1b9d472
ap-riksa-pradipta <riksa.pradipta@astrapay.com>
2025-06-11 09:16:27 +0700

    delete innerhtml

--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
-// Add Content-Security-Policy
-document.getElementsByTagName(
-  "head"
-)[0].innerHTML += `<meta http-equiv="Content-Security-Policy" content="default-src gap://ready file://* *; style-src 'self' http://* https://* 'unsafe-inline'; script-src 'self' http://* https://* 'unsafe-inline' 'unsafe-eval';">`;
 
 // Initialize variables
 let tabs = [];
